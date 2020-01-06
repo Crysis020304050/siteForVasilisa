d382040333101ab8f48459444856549373757053
Crysis020304050 <crysiswarhead020304050@gmail.com>
2020-01-06 11:09:26 +0200

    Add a full screen view work picture in mobile version

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-'use strict';
 
 import './assets/css/reset.css';
 import './assets/scss/styles.scss';
 
 import './assets/js/scrollNavigationEvents';
 import './assets/js/openMobileNavigation';
-import './assets/js/openWorkPhoto';
 import './assets/js/teamsLoader';
 import './assets/js/slider';
+import './assets/js/openWorkPhoto';
 
 
 
